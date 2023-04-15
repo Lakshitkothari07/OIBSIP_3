4476d70b3ba2accecf195fba3c280fb79842a771
Lakshitkothari07 <121547370+Lakshitkothari07@users.noreply.github.com>
2023-04-15 09:51:51 +0530

    Update Car_Price_Prediction.py

--- Car_Price_Prediction.py
+++ Car_Price_Prediction.py
@@ -35,3 +35,38 @@ plt.show()
 
 print(data.corr())
 
+plt.figure(figsize=(20, 15))
+correlation = data.corr()
+sns.heatmap(correlation, cmap = "coolwarm", annot=True)
+plt.show()
+
+# Training a Car Prediction Model
+# I use the decision tree regression algorithm to train a car price prediction model. So let's split the data into training and test sets and use the decision tree regression to train the model
+
+predict = "price"
+data = data[[ "symboling" , "wheelbase" , "carlength",
+              "carwidth" , "carheight" , "curbweight",
+              "enginesize" , "boreratio" , "stroke",
+              "compressionratio" , "horsepower" , "peakrpm",
+              "citympg" , "highwaympg" , "price"]]
+x = np.array(data.drop([predict],  1))
+y = np.array(data[predict])
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = 0.2)
+
+from sklearn.tree import DecisionTreeRregressor
+model = DecisionTreeRegressor()
+model.fit(xtrain, ytrain)
+predictions = model.predict(xtest)
+
+from sklearn.metrics import mean_absolute_error
+model.score(xtest, predictions)
+
+
+# The model gives 100% accuracy on the test set, which is excellent.
+# so this is how you can train a machine learning model for the task of predicting car prices by using the Python programming language. 
+
+
+
+
